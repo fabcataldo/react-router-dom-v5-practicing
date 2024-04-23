adf4265a5cd25470beb160a86c2d633fb2ccf8e4
Fabio Cataldo <fabioalbertocataldo@gmail.com>
2024-04-22 22:05:14 -0300

    Simple lazyload (one page routing config setted) developed

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -8,44 +8,48 @@ import {
 
 import logo from '../logo.svg';
 import { routes } from './routes';
+import { Suspense } from 'react';
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React Logo" />
-          <ul>
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Router>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              {
+                routes.map(({ path, name }) => (
+                  <li key={path}>
+                    <NavLink
+                      to={path}
+                      activeClassName="nav-active"
+                      exact>
+                      {name}
+                    </NavLink>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
             {
-              routes.map(({path, name}) => (
-                <li key={path}>
-                  <NavLink
-                    to={path}
-                    activeClassName="nav-active"
-                    exact>
-                    {name}
-                  </NavLink>
-                </li>
+              routes.map(({ path, component: Component }) => (
+                <Route
+                  path={path}
+                  key={path}
+                  render={() => <Component></Component>}>
+                </Route>
               ))
             }
-          </ul>
-        </nav>
+            <Redirect to={routes[0].path} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
 
-        {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-        <Switch>
-          {
-            routes.map(({path, component: Component}) => (
-              <Route
-                path={path}
-                key={path}
-                render={() => <Component></Component>}>
-              </Route>
-            ))
-          }
-          <Redirect to={routes[0].path}/>
-        </Switch>
-      </div>
-    </Router>
   );
 }
